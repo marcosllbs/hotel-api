028be12d4dc43f6ccbd80d896959adf2794a2f2b
Marcos Souza <marcosllbs@hotmail.com>
2024-10-19 00:29:47 -0300

    aspNetcore MVC

--- obj/Debug/net8.0/HotelAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/HotelAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e2ab4b393e9378789eb278ad6c6eda6a47a4b3c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6f9af625cdaf2a4ffac8a8cb9d2064159eb2777")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
