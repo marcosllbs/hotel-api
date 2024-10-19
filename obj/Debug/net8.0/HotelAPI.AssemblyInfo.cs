88f449900a3c1b3c8c4499315d9932de4dec41c5
Marcos Souza <marcosllbs@hotmail.com>
2024-10-19 00:01:03 -0300

    Entity Framework addittion

--- obj/Debug/net8.0/HotelAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/HotelAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8e9be6563abb95fe66f726e8e158d2ad95a5dc1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+109e934d040434cc97143e271603b3734ca968f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
